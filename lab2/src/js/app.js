493a23a8652b175389cf7fa5ed4da16fb3ccbf6d
Dowon Lee <edowon0623@gmail.com>
2019-08-29 16:47:26 +0900

    upload final sources#

--- lab2/src/js/app.js
+++ lab2/src/js/app.js
@@ -34,9 +34,10 @@ App = {
   },
 
   initContract: function() {
-	$.getJSON('RealEstate.json', function(data) {
+		$.getJSON('RealEstate.json', function(data) {
       App.contracts.RealEstate = TruffleContract(data);
       App.contracts.RealEstate.setProvider(App.web3Provider);
+      App.listenToEvents();
     });
   },
 
@@ -44,7 +45,7 @@ App = {
     var id = $('#id').val();
     var name = $('#name').val();
     var price = $('#price').val();
-    var email = $('#email').val();
+    var age = $('#age').val();
 
     web3.eth.getAccounts(function(error, accounts) {
       if (error) {
@@ -54,11 +55,12 @@ App = {
       var account = accounts[0];
       App.contracts.RealEstate.deployed().then(function(instance) {
         var nameUtf8Encoded = utf8.encode(name);
-        return instance.buyRealEstate(id, web3.toHex(nameUtf8Encoded), email, { from: account, value: price });
+        //return instance.buyRealEstate(id, web3.toHex(nameUtf8Encoded), age, { from: account, value: price });
+        return instance.buyRealEstate(id, nameUtf8Encoded, age, { from: account, value: price });
       }).then(function() {
         $('#name').val('');
-        $('#email').val('');
-        $('#buyModal').modal('hide');       
+        $('#age').val('');
+        $('#buyModal').modal('hide');    
       }).catch(function(err) {
         console.log(err.message);
       });
@@ -66,11 +68,45 @@ App = {
   },
 
   loadRealEstates: function() {
-	
+    App.contracts.RealEstate.deployed().then(function(instance) {
+      return instance.getAllBuyers.call();
+    }).then(function(buyers) {
+      for (i = 0; i < buyers.length; i++) {
+        if (buyers[i] !== '0x0000000000000000000000000000000000000000') { // 0이 40개 (초기화)
+          var imgType = $('.panel-realEstate').eq(i).find('img').attr('src').substr(7);
+
+          switch(imgType) {
+            case 'apartment.jpg':
+              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/apartment_sold.jpg')
+              break;
+            case 'townhouse.jpg':
+              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/townhouse_sold.jpg')
+              break;
+            case 'house.jpg':
+              $('.panel-realEstate').eq(i).find('img').attr('src', 'images/house_sold.jpg')
+              break;
+          }
+
+          $('.panel-realEstate').eq(i).find('.btn-buy').text('매각').attr('disabled', true);
+          $('.panel-realEstate').eq(i).find('.btn-buyerInfo').removeAttr('style');
+        }
+      }
+    }).catch(function(err) {
+      console.log(err.message);
+    })
   },
 	
   listenToEvents: function() {
-	
+    App.contracts.RealEstate.deployed().then(function(instance) {
+      instance.LogBuyRealEstate({filter:{}, fromBlock:0, toBlock:'latest'}, function(error, event) {
+        if (!error) {
+          $('#events').append('<p>' + event.args._buyer + ' 계정에서 ' + event.args._id + ' 번 매물을 매입했습니다.' + '</p>');
+        } else {
+          console.error(error);
+        }
+        App.loadRealEstates();
+      })
+    })
   }
 };
 
@@ -85,5 +121,22 @@ $(function() {
 
     $(e.currentTarget).find('#id').val(id);
     $(e.currentTarget).find('#price').val(price);
+  });
+
+  $('#buyerInfoModal').on('show.bs.modal', function(e) {
+    var id = $(e.relatedTarget).parent().find('.id').text();
+   
+    App.contracts.RealEstate.deployed().then(function(instance) {
+      return instance.getBuyerInfo.call(id);
+    }).then(function(buyerInfo) {
+
+      var nameUtf8Decoded = utf8.decode(buyerInfo[1]);
+      
+      $(e.currentTarget).find('#buyerAddress').text(buyerInfo[0]);
+      $(e.currentTarget).find('#buyerName').text(nameUtf8Decoded);
+      $(e.currentTarget).find('#buyerAge').text(buyerInfo[2]);
+    }).catch(function(err) {
+      console.log(err.message);
+    })
   });
 });
